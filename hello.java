cc8164c55552cfd6c16248d27550ca542ac0946c
root <root@ip-172-31-30-124.us-east-2.compute.internal>
2021-07-19 14:07:06 +0000

    conflict resolved

--- hello.java
+++ hello.java
@@ -2,11 +2,9 @@ class Hello
 {
 public static void main(String args[])
 {
-<<<<<<< Updated upstream
-System.out.println("hello changes done in repo");
-=======
+
 System.out.println("hello change should be on master");
->>>>>>> Stashed changes
+
 }
 }
 
